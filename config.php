55ade70b05270a221f7c36f53facafdc5bd6ee3d
Jacob Parker <Djmann1013@gmail.com>
2014-05-10 21:57:49 +0000

    Added the config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// Config file
+
+// Server's IP
+$server_ip = "aos://1379434439:42887";
+
+// Master Server
+$resource = "http://services.buildandshoot.com/serverlist.json";
+
+// Color of "Offline" message
+$offline_color = "FF0400";
+
+// Color of "Online" message
+$online_color = "00FF1E";
+
+// Custom message when server is offline
+$offline_message = "Server Offline!";
+
+// Custom message when the server is online
+$online_message = "Server Online!";
+
+// Custom "Join Server" message
+$join_message = "<img src='http://i.imgur.com/3GinYaH.png' />";
+
+?>
