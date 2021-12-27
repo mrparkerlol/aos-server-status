1ebdfcaf088f5d71b08712e9ca1050beb446ac08
Jacob Parker <contact@mrparker.dev>
2021-12-27 12:35:02 -0800

    Restructure a tiny bit, fix demo

--- server_status.js
+++ server_status.js
@@ -1,8 +1,8 @@
-let aos_server_status = async (server_ip, server_list) => {
+async function aos_server_status(server_ip, server_list) {
     server_ip = server_ip.replace(/:[0-9]\.[0-9]+$/, "") // removes version info (0.75, etc), which isn't needed for identifier matching
-    let server_list = await (await fetch(server_list != undefined ? server_list : "https://services.buildandshoot.com/serverlist.json")).json()
+    server_list_results = await (await fetch(server_list != undefined ? server_list : "https://services.buildandshoot.com/serverlist.json")).json()
     let data = undefined
-    server_list.forEach(server => {
+    server_list_results.forEach(server => {
         if (server.identifier == server_ip) {
             data = server
         }
