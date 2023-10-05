70c58e1948ea905f770de6d6e4f97f9c80d3a49b
Jacob Parker <2037716+mrparkerlol@users.noreply.github.com>
2023-10-05 03:48:33 -0700

    Whoops - fixed.

--- server_status.js
+++ server_status.js
@@ -1,7 +1,7 @@
 async function aos_server_status(server_ip, server_list) {
     server_ip = server_ip.replace(/:[0-9]\.[0-9]+$/, "") // removes version info (0.75, etc), which isn't needed for identifier matching
     server_list_results = await (await fetch(server_list != undefined ? server_list : "https://services.buildandshoot.com/serverlist.json")).json()
-    for (let server in server_list_results) {
+    for (let server of server_list_results) {
         if (server.identifier == server_ip) {
             return server
         }
