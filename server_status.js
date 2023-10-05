b14b614dc798767fc50d88ad212b6656f6c76cfe
Jacob Parker <2037716+mrparkerlol@users.noreply.github.com>
2023-10-05 03:38:41 -0700

    Shorten down server_status.js a little bit

--- server_status.js
+++ server_status.js
@@ -1,12 +1,9 @@
 async function aos_server_status(server_ip, server_list) {
     server_ip = server_ip.replace(/:[0-9]\.[0-9]+$/, "") // removes version info (0.75, etc), which isn't needed for identifier matching
     server_list_results = await (await fetch(server_list != undefined ? server_list : "https://services.buildandshoot.com/serverlist.json")).json()
-    let data = undefined
-    server_list_results.forEach(server => {
+    for (let server in server_list_results) {
         if (server.identifier == server_ip) {
-            data = server
+            return server
         }
-    })
-
-    return data
+    }
 }
